0a5885ea40279bba79489e211f39dc7416ac16dd
Maulana Ibrahim <maulputra09@gmail.com>
2023-08-19 14:40:15 +0700

    fixing more bugs

--- controller/includes.js
+++ controller/includes.js
@@ -1,14 +1,15 @@
 const { createTransport } = require('nodemailer') 
 const userEmail = require('../models/mailer')
 require('dotenv').config({});
+
 const transporter = createTransport({
     host: "smtp-relay.brevo.com",
     port: 587,
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
     }
-});
+  });
 
 exports.postEmail = async (req,res,next) => {
     const email = req.body.email
@@ -34,23 +35,23 @@ exports.postEmail = async (req,res,next) => {
             await user.save()
         }
 
-      await new Promise((resolve, reject) => {
-    transporter.sendMail({
-        from: 'hallobutter@gmail.com',
+        const mailOptions = {
+            from: 'hallobutter@gmail.com',
             to: email,
             subject: 'Promo Spesial: Nikmati Diskon Menarik untuk Pembelian Kue di Hari Ulang Tahun!',
             html : `<p>Kami sangat senang mendengar bahwa Anda akan merayakan ulang tahun Anda! Kami ingin berbagi dalam kebahagiaan Anda dengan penawaran spesial dari Hellobutter. Pada tanggal ${day}-${month}-${nextYear} kami akan memberikan diskon eksklusif untuk pembelian kue di Hari Ulang Tahun Anda. Kami berharap ini dapat menambah kesenangan pada momen spesial Anda.</p>`
-    },
-     (err, info) => {
-      if (err) {
-        console.error(err);
-        reject(err);
-      } else {
-        resolve(info);
-      }
-    });
-  });
-  return res.redirect('/')
+                }
+                await new Promise((resolve, reject) => {
+                    transporter.sendMail(mailOptions, (err, info) => {
+                      if (err) {
+                        console.error(err);
+                        reject(err);
+                      } else {
+                        resolve(info);
+                      }
+                    });
+                  });
+  res.redirect('/')
     }
     catch(err) {
         next(err)
